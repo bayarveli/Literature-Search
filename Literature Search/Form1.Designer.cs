d13f82d96a6071a0f6d74ba2ad9f35aa3d461c1e
Veli BAYAR <bayar.veli@gmail.com>
2015-11-24 14:28:02 +0200

    Project files are added.

---
+++ Literature Search/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace Literature_Search
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fbr_Dosya_Tarayici_Diyalog = new System.Windows.Forms.FolderBrowserDialog();
+            this.tb_Secilen_Dosya_Yolu = new System.Windows.Forms.TextBox();
+            this.btn_Klasor_Sec = new System.Windows.Forms.Button();
+            this.lb_Dosyalar = new System.Windows.Forms.ListBox();
+            this.chb_pdf = new System.Windows.Forms.CheckBox();
+            this.chb_txt = new System.Windows.Forms.CheckBox();
+            this.chb_doc = new System.Windows.Forms.CheckBox();
+            this.btn_Excele_Aktar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tb_Secilen_Dosya_Yolu
+            // 
+            this.tb_Secilen_Dosya_Yolu.Location = new System.Drawing.Point(131, 15);
+            this.tb_Secilen_Dosya_Yolu.Name = "tb_Secilen_Dosya_Yolu";
+            this.tb_Secilen_Dosya_Yolu.Size = new System.Drawing.Size(457, 20);
+            this.tb_Secilen_Dosya_Yolu.TabIndex = 0;
+            // 
+            // btn_Klasor_Sec
+            // 
+            this.btn_Klasor_Sec.Location = new System.Drawing.Point(12, 12);
+            this.btn_Klasor_Sec.Name = "btn_Klasor_Sec";
+            this.btn_Klasor_Sec.Size = new System.Drawing.Size(113, 24);
+            this.btn_Klasor_Sec.TabIndex = 1;
+            this.btn_Klasor_Sec.Text = "Klasör Seç";
+            this.btn_Klasor_Sec.UseVisualStyleBackColor = true;
+            this.btn_Klasor_Sec.Click += new System.EventHandler(this.btn_Klasor_Sec_Click);
+            // 
+            // lb_Dosyalar
+            // 
+            this.lb_Dosyalar.FormattingEnabled = true;
+            this.lb_Dosyalar.Location = new System.Drawing.Point(13, 91);
+            this.lb_Dosyalar.Name = "lb_Dosyalar";
+            this.lb_Dosyalar.ScrollAlwaysVisible = true;
+            this.lb_Dosyalar.Size = new System.Drawing.Size(575, 225);
+            this.lb_Dosyalar.TabIndex = 2;
+            // 
+            // chb_pdf
+            // 
+            this.chb_pdf.AutoSize = true;
+            this.chb_pdf.Location = new System.Drawing.Point(13, 68);
+            this.chb_pdf.Name = "chb_pdf";
+            this.chb_pdf.Size = new System.Drawing.Size(44, 17);
+            this.chb_pdf.TabIndex = 3;
+            this.chb_pdf.Text = ".pdf";
+            this.chb_pdf.UseVisualStyleBackColor = true;
+            // 
+            // chb_txt
+            // 
+            this.chb_txt.AutoSize = true;
+            this.chb_txt.Location = new System.Drawing.Point(63, 68);
+            this.chb_txt.Name = "chb_txt";
+            this.chb_txt.Size = new System.Drawing.Size(40, 17);
+            this.chb_txt.TabIndex = 4;
+            this.chb_txt.Text = ".txt";
+            this.chb_txt.UseVisualStyleBackColor = true;
+            // 
+            // chb_doc
+            // 
+            this.chb_doc.AutoSize = true;
+            this.chb_doc.Location = new System.Drawing.Point(109, 68);
+            this.chb_doc.Name = "chb_doc";
+            this.chb_doc.Size = new System.Drawing.Size(78, 17);
+            this.chb_doc.TabIndex = 5;
+            this.chb_doc.Text = ".doc/.docx";
+            this.chb_doc.UseVisualStyleBackColor = true;
+            // 
+            // btn_Excele_Aktar
+            // 
+            this.btn_Excele_Aktar.Location = new System.Drawing.Point(461, 322);
+            this.btn_Excele_Aktar.Name = "btn_Excele_Aktar";
+            this.btn_Excele_Aktar.Size = new System.Drawing.Size(123, 23);
+            this.btn_Excele_Aktar.TabIndex = 6;
+            this.btn_Excele_Aktar.Text = "Excel\'e Aktar";
+            this.btn_Excele_Aktar.UseVisualStyleBackColor = true;
+            this.btn_Excele_Aktar.Click += new System.EventHandler(this.btn_Excele_Aktar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(596, 352);
+            this.Controls.Add(this.btn_Excele_Aktar);
+            this.Controls.Add(this.chb_doc);
+            this.Controls.Add(this.chb_txt);
+            this.Controls.Add(this.chb_pdf);
+            this.Controls.Add(this.lb_Dosyalar);
+            this.Controls.Add(this.btn_Klasor_Sec);
+            this.Controls.Add(this.tb_Secilen_Dosya_Yolu);
+            this.Name = "Form1";
+            this.Text = "Literature Search List";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog fbr_Dosya_Tarayici_Diyalog;
+        private System.Windows.Forms.TextBox tb_Secilen_Dosya_Yolu;
+        private System.Windows.Forms.Button btn_Klasor_Sec;
+        private System.Windows.Forms.ListBox lb_Dosyalar;
+        private System.Windows.Forms.CheckBox chb_pdf;
+        private System.Windows.Forms.CheckBox chb_txt;
+        private System.Windows.Forms.CheckBox chb_doc;
+        private System.Windows.Forms.Button btn_Excele_Aktar;
+    }
+}
+
---
+++ Literature Search/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using Excel = Microsoft.Office.Interop.Excel;
+
+
+namespace Literature_Search
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        List<string> s_dosyalar = new List<string>();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_Klasor_Sec_Click(object sender, EventArgs e)
+        {
+            lb_Dosyalar.Items.Clear();
+            s_dosyalar.Clear();
+
+            DialogResult result = fbr_Dosya_Tarayici_Diyalog.ShowDialog();
+            
+
+            string dosya_uzantisi;
+            string dosya_adi;
+
+            if (result == DialogResult.OK)
+            {
+                if (chb_pdf.Checked == false &&
+                    chb_txt.Checked == false &&
+                    chb_doc.Checked == false)
+                {
+                    chb_pdf.Checked = true;
+                    MessageBox.Show("Seçim yapılmadığı için, varsayılan dosya tipi .pdf olarak seçilmiştir.");
+                }
+                
+                tb_Secilen_Dosya_Yolu.Text = fbr_Dosya_Tarayici_Diyalog.SelectedPath;
+
+                string[] s_dosya_yollari = Directory.GetFiles(fbr_Dosya_Tarayici_Diyalog.SelectedPath);
+
+                for (int i = 0; i < s_dosya_yollari.Length; i++)
+                {
+                    dosya_uzantisi = Path.GetExtension(s_dosya_yollari[i]);
+                    dosya_adi = Path.GetFileName(s_dosya_yollari[i]);
+
+                    if (chb_pdf.Checked == true && dosya_uzantisi == ".pdf")
+                    {
+
+                        s_dosyalar.Add(dosya_adi);
+                    }
+                    
+                    if (chb_txt.Checked == true && dosya_uzantisi == ".txt")
+                    {
+                        s_dosyalar.Add(dosya_adi);
+                    }
+                    
+                    if (chb_doc.Checked == true && (dosya_uzantisi == ".doc" || dosya_uzantisi == ".docx"))
+                    {
+                        s_dosyalar.Add(dosya_adi);
+                    }       
+                }
+                
+                lb_Dosyalar.Items.AddRange(s_dosyalar.ToArray());
+            }
+            else
+            {
+                MessageBox.Show("Seçilen dosya yolu geçersizdir.");
+            }
+        }
+
+        private void btn_Excele_Aktar_Click(object sender, EventArgs e)
+        {
+            Microsoft.Office.Interop.Excel.Application xlApp;
+            Microsoft.Office.Interop.Excel.Workbook xlWorkBook;
+            Microsoft.Office.Interop.Excel.Worksheet xlWorkSheet;
+
+            object misValue = System.Reflection.Missing.Value;
+
+            xlApp = new Microsoft.Office.Interop.Excel.ApplicationClass();
+            xlWorkBook = xlApp.Workbooks.Add(misValue);
+            xlWorkSheet = (Microsoft.Office.Interop.Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1);
+            
+            for (int i = 0; i < lb_Dosyalar.Items.Count; i++)
+            {
+                xlWorkSheet.Cells[i + 2, 2] = lb_Dosyalar.Items[i].ToString();
+                
+                //Dosyaya hyperlink at
+                xlWorkSheet.Hyperlinks.Add(xlWorkSheet.Cells[i + 2, 2], tb_Secilen_Dosya_Yolu.Text + "/" + lb_Dosyalar.Items[i].ToString());
+            }
+            
+            string kaydedilecek_dosya_yolu = fbr_Dosya_Tarayici_Diyalog.SelectedPath + "/literatur.xls";
+
+            xlWorkBook.SaveAs(kaydedilecek_dosya_yolu, Microsoft.Office.Interop.Excel.XlFileFormat.xlWorkbookNormal, misValue, misValue, misValue, misValue, Microsoft.Office.Interop.Excel.XlSaveAsAccessMode.xlExclusive, misValue, misValue, misValue, misValue, misValue);
+            xlWorkBook.Close(true, misValue, misValue);
+            xlApp.Quit();
+            releaseObject(xlWorkSheet);
+            releaseObject(xlWorkBook);
+            releaseObject(xlApp);
+        }
+
+        private void releaseObject(object obj)
+        {
+            try
+            {
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(obj);
+                obj = null;
+            }
+            catch (Exception ex)
+            {
+                obj = null;
+                MessageBox.Show("Exception Occured while releasing object " + ex.ToString());
+            }
+            finally
+            {
+                GC.Collect();
+            }
+        }
+    }
+}
